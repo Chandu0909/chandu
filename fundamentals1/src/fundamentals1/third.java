640bde04ab98103f4bfe8fbc5d26d80d3b5b6fb5
Personal <Personal@DESKTOP-L12EV7F>
2019-06-25 12:24:57 +0530

    eg3

---
+++ fundamentals1/src/fundamentals1/third.java
@@ -0,0 +1,14 @@
+package fundamentals1;
+
+public class third {
+
+	public static void main(String[] args) {
+		int x,y,z;
+		x=Integer.parseInt(args[0]);
+		y=Integer.parseInt(args[1]);
+		z=x+y;
+		System.out.println( " sum of " +x+ " and " +y+ " is " +z);
+
+	}
+
+}
