590479788ecd412659014cdb375810e312074c51
Personal <Personal@DESKTOP-L12EV7F>
2019-06-26 12:13:24 +0530

    eg11

---
+++ fundamentals1/src/fundamentals1/eleven.java
@@ -0,0 +1,15 @@
+package fundamentals1;
+
+public class eleven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		for(i=1;i<=10;i++)
+		{
+			System.out.print(i+"\t");
+		}
+
+	}
+
+}
