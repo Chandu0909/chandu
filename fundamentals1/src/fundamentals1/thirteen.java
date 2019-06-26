4bb8be89862ba6507b0b723609465f2f8a919f7a
Personal <Personal@DESKTOP-L12EV7F>
2019-06-26 12:22:21 +0530

    eg13

---
+++ fundamentals1/src/fundamentals1/thirteen.java
@@ -0,0 +1,24 @@
+package fundamentals1;
+
+public class thirteen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=0,j=0,count=0;
+		for(i=10;i<=99;i++)
+		{
+			count=0;
+			for(j=1;j<=i;j++)
+			{
+				if(i%j==0)
+					count++;
+			}
+			if(count==2)
+				System.out.println(i);
+		}
+		
+	}
+
+	}
+
