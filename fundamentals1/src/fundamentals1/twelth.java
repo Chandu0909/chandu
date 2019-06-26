b48cc9953ba2195b3d956b28caa0c71285e345f8
Personal <Personal@DESKTOP-L12EV7F>
2019-06-26 12:17:30 +0530

    eg12

---
+++ fundamentals1/src/fundamentals1/twelth.java
@@ -0,0 +1,22 @@
+package fundamentals1;
+
+public class twelth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=0,count=0,num=Integer.parseInt(args[0]);
+		
+		for(i=1;i<=num;i++)
+		{
+		
+			if(num%i==0)
+				count++;
+		}
+		
+		if(count==2)
+			System.out.println("Given Number "+num+" is Prime");
+		else
+			System.out.println("Given Number "+num+" is Not Prime");
+	}
+
+	}
